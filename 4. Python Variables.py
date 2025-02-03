48dd3dbb2cc627114efac340e594df45aa10e763
Baltag <uie78384@contiwan.com>
2025-02-03 16:16:12 +0200

    Completed Python variables until Variable Names

---
+++ 4. Python Variables.py
@@ -0,0 +1,36 @@
+# Variables are used for storing data value
+# Variable is created when you first assign a value to it
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#Casting
+x = str(3)      #x will be 3
+y = int(3)      #y will be 3
+z = float(3)    #z will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+#Get the type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#Single or double quotes? - String variables can be declared either
+x = "John"
+# is the same as
+x = 'John'
+# Variable names are canse sensitive
+a = 4
+A = "Sally"
+# a will not overwrite A fyi
+print(a)
+print(A)
---
+++ 4.1. Variable Names.py
@@ -0,0 +1,23 @@
+#legal variable names
+myvar = "John"
+my_var = "John"
+_my-var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+"""illegal variable names
+2myvar = "John"
+my-var = "John"
+my var= "John"
+"""
+# VARIABLE NAMES ARE CASE SENSITIVE
+# Techniques to read multiword variables
+
+#Camel case:
+myVariableName = "John"
+
+#Pascal case:
+MyVariableName = "John"
+
+#Snake case:
+my_variable_name = "John"

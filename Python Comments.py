0033bca8a376805e09b339cc2c59f505b7feae05
Baltag <uie78384@contiwan.com>
2025-02-03 15:53:38 +0200

    Added completed Python Comments.py

---
+++ Python Comments.py
@@ -0,0 +1,18 @@
+#This is a comment
+print("Hello, world!")
+
+print("Hello, World!") #YOU CAN PLACE COMMENTS ANYWHERE
+
+#print("Hello, World!") #PREVENT EXECUTING THE CODE
+
+#Comment
+#Written
+#more than just a line
+print("Hello, world!")
+
+"""
+Multiline 
+String
+written in more than just a line
+"""
+print("Hello, World!")

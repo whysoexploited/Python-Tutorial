62de4147a9f5c63effafa77b5c7b44528cfac6a2
Baltag <cristi.baltag15@gmail.com>
2025-03-19 16:40:46 +0200

    Completed tutorial ----> Python Math

---
+++ Math.py
@@ -0,0 +1,24 @@
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x, y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)
+print(x)
+
+x = math.sqrt(64)
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+x = math.pi
+print(x)
